6a1f4ab6e9a7ff0cdbb82d53dab1415e1340f184
angele34 <90606365+angele34@users.noreply.github.com>
2024-10-24 22:27:17 +0800

    implement Sell function and fix space padding

--- src/menu.c
+++ src/menu.c
@@ -7,22 +7,25 @@
 void DisplayMenu(int player_code, int initial_capital, int target_profit);
 void DisplayMainScreen(int player_code, int initial_capital, int target_profit);
 void Buy(int player_code, int initial_capital, int target_profit);
+void Sell(int player_code, int initial_capital, int target_profit);
 void DisplayNavigationScreen(int player_code, int initial_capital, int target_profit);
 
 // TODO: Optimize the key press code
 
 // Displays the main menu containing player data and progress
 void DisplayMenu(int player_code, int initial_capital, int target_profit) {
+    int current_profit = 0, day = 0;
     printf("\n\nMain Screen\n\n\n");
 
-    printf("Player %d \t\t\t\t Gold Coins: %d\n", player_code, initial_capital);
-    printf("Location: Manila \t\t\t\t Profit: \n", target_profit);
-    printf("Day %d\n\n\n\n", 0);
+    printf("Player: %03d \t\t\t\t Gold Coins: %d\n", player_code, initial_capital);
+    printf("Location: Manila \t\t\t Profit: %5d/%d%%\n", current_profit, target_profit);
+    printf("Day %d\n\n\n\n", day);
 }
 
 // Displays the main screen containing cargo and market prices
 void DisplayMainScreen(int player_code, int initial_capital, int target_profit) {
-    printf("   Cargo \t 0 of 75\n");
+    int cargo = 0;
+    printf("   Cargo \t%3d of 75\n", cargo);
     printf("   =======================\n");
     printf("   Coconut - 0   Silk - 0\n");
     printf("   Rice    - 0   Gun  - 0\n\n\n");
@@ -35,11 +38,11 @@ void DisplayMainScreen(int player_code, int initial_capital, int target_profit) 
     printf("   Rice    - 0   Gun  - 0\n\n\n  ");
 
     printf("What would you like to do?\n");
-    printf("\t[1] Buy\n");
-    printf("\t[2] Sell\n");
-    printf("\t[3] Go to Another Port\n\n");
+    printf("%*s%s", 4, "", "[1] Buy\n");
+    printf("%*s%s", 4, "", "[2] Sell\n");
+    printf("%*s%s", 4, "", "[3] Go to Another Port\n\n");
 
-    printf("\t[Q] Quit\n\n\n");
+    printf("%*s%s", 4, "", "[Q] Quit\n\n\n");
 
     while (1) {
         char key = getch();
@@ -47,16 +50,20 @@ void DisplayMainScreen(int player_code, int initial_capital, int target_profit) 
             #ifdef _WIN32 
                 system("cls");
             #endif
-            DisplayMenu(player_code, initial_capital, target_profit);
-            Buy(player_code, initial_capital, target_profit);
+                DisplayMenu(player_code, initial_capital, target_profit);
+                Buy(player_code, initial_capital, target_profit);
         } else if (key == '2') {
-            printf("Selling\n");
+            #ifdef _WIN32 
+                system("cls");
+            #endif
+                DisplayMenu(player_code, initial_capital, target_profit);
+                Sell(player_code, initial_capital, target_profit);
         } else if (key == '3') {
             #ifdef _WIN32 
                 system("cls");
             #endif
-            DisplayMenu(player_code, initial_capital, target_profit);
-            DisplayNavigationScreen(player_code, initial_capital, target_profit);
+                DisplayMenu(player_code, initial_capital, target_profit);
+                DisplayNavigationScreen(player_code, initial_capital, target_profit);
         } else if (key == 'q' || key == 'Q') {
             exit(0);
         }
@@ -66,12 +73,12 @@ void DisplayMainScreen(int player_code, int initial_capital, int target_profit) 
 // Displays the buy screen upon pressing [1] Buy
 void Buy(int player_code, int initial_capital, int target_profit) {
     printf("What would you like to buy?\n");
-    printf("\t[1] Coconut\n");
-    printf("\t[2] Rice\n");
-    printf("\t[2] Silk\n");
-    printf("\t[2] Gun\n\n\n");
+    printf("%*s%s", 2, "", "[1] Coconut\n");
+    printf("%*s%s", 2, "", "[2] Rice\n");
+    printf("%*s%s", 2, "", "[3] Silk\n");
+    printf("%*s%s", 2, "", "[4] Gun\n\n\n");
 
-    printf("\t[X] Return to the Main Screen\n\n\n");
+    printf("%*s%s", 2, "", "[X] Return to the Main Screen\n\n\n");
 
     while(1) {
         char key = getch();
@@ -96,16 +103,47 @@ void Buy(int player_code, int initial_capital, int target_profit) {
     }
 }
 
+void Sell(int player_code, int initial_capital, int target_profit) {
+    printf("What would you like to sell?\n");
+    printf("%*s%s", 2, "", "[1] Coconut\n");
+    printf("%*s%s", 2, "", "[2] Rice\n");
+    printf("%*s%s", 2, "", "[3] Silk\n");
+    printf("%*s%s", 2, "", "[4] Gun\n\n\n");
+
+    printf("%*s%s", 2, "", "[X] Return to the Main Screen\n\n\n");
+
+    while(1) {
+        char key = getch();
+        if (key == '1') {
+            printf("Selling Coconut\n");
+        } else if (key == '2') {
+            printf("Selling Rice\n");
+        } else if (key == '3') {
+            #ifdef _WIN32 
+                system("cls");
+            #endif
+            DisplayMainScreen(player_code, initial_capital, target_profit);
+        } else if (key == '4') {
+            printf("Selling Gun\n");
+        } else if (key == 'x' || key == 'X') {
+            #ifdef _WIN32 
+                system("cls");
+            #endif
+            DisplayMenu(player_code, initial_capital, target_profit);
+            DisplayMainScreen(player_code, initial_capital, target_profit);
+        }
+    }
+}
 
 
 // Displays the navigation screen upon pressing [3] Go to Another Port
 void DisplayNavigationScreen(int player_code, int initial_capital, int target_profit) {
     printf("Where would you like to go?\n");
-    printf("\t[1] Tondo\n");
-    printf("\t[2] Pandakan\n");
-    printf("\t[3] Sapa\n\n\n");
+    printf("%*s%s", 2, "", "[1] Tondo\n");
+    printf("%*s%s", 2, "", "[2] Pandakan\n");
+    printf("%*s%s", 2, "", "[3] Sapa\n\n\n");
 
-    printf("\t[X] Return to the Main Screen\n\n\n");
+    printf("%*s%s", 2, "", "[X] Return to the Main Screen\n\n\n");
 
     while(1) {
         char key = getch();
